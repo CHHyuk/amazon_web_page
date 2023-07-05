12d61845b0e82ba477178ddc4f26f2b44edf7740
CHHyuk <gksgur1038@naver.com>
2023-07-05 20:32:15 +0900

    제품 props 입력

--- src/Home.js
+++ src/Home.js
@@ -8,15 +8,43 @@ function Home() {
             <div className="home_container">
                 <img className="home_image" src="https://images.idgesg.net/images/article/2017/09/firetvad2-100736366-orig.jpg" alt="" />
                 <div className="home_row">
-                    <Product />
+                    <Product id="2323"
+                        title="제품 1"
+                        price={3000}
+                        image="https://picsum.photos/id/250/292/158"
+                        rating={5} />
+
+                    <Product id="2320"
+                        title="제품 2"
+                        price={200}
+                        image="https://picsum.photos/id/232/292/158"
+                        rating={3} />
                 </div>
                 <div className="home_row">
-                    <Product />
-                    <Product />
-                    <Product />
+                    <Product id="2321"
+                        title="제품 3"
+                        price={5000}
+                        image="https://picsum.photos/id/12/292/158"
+                        rating={1} />
+
+                    <Product id="2322"
+                        title="제품 4"
+                        price={2143500}
+                        image="https://picsum.photos/id/24/292/158"
+                        rating={4} />
                 </div>
                 <div className="home_row">
-                    <Product />
+                    <Product id="2324"
+                        title="제품 5"
+                        price={56400}
+                        image="https://picsum.photos/id/55/292/158"
+                        rating={3} />
+
+                    <Product id="2325"
+                        title="제품 6"
+                        price={2143500}
+                        image="https://picsum.photos/id/240/292/158"
+                        rating={4} />
                 </div>
                 <div className="home_row">
                     <Product />
--- src/Product.js
+++ src/Product.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import "./Product.css";
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
+
+
     return (
         <div className='product'>
             <div className='product_info'>
-                <p>제품1</p>
+                <p>{title}</p>
                 <p className='product_price'>
-                    <small>가격</small>
-                    <strong>10,000</strong>
-                    <small>원</small>
+                    <small>가격 </small>
+                    <strong>{price}</strong>
+                    <small> 원</small>
                 </p>
 
                 <div className='product_rating'>
-                    <p>★</p>
-                    <p>★</p>
-                    <p>★</p>
-                    <p>★</p>
+                    {
+                        Array(rating)
+                            .fill()
+                            .map(() => (
+                                <p>★</p>
+                            ))
+                    }
                 </div>
             </div>
 
-            <img src="https://images.idgesg.net/images/article/2017/09/firetvad2-100736366-orig.jpg" alt="사진"/>
+            <img src={image} alt="사진" />
             <button>장바구니에 담기</button>
         </div>
     )
