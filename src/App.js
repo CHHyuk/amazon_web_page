08a545c9f0d3396f2177c2ce8e539da58a84eae7
CHHyuk <gksgur1038@naver.com>
2023-07-05 13:57:09 +0900

    빌드, firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from "react";
+import './App.css';
 
 function App() {
-  return <></>
+  return (
+  <div className='app'>
+    <h1>헤더</h1>
+    <h1>본문</h1>  
+  </div>
+  );
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAIE8R1Qw0Y1nVmeQNZVfwzU9_RT88Gkf8",
+    authDomain: "fir-9fb2b.firebaseapp.com",
+    projectId: "fir-9fb2b",
+    storageBucket: "fir-9fb2b.appspot.com",
+    messagingSenderId: "385838623346",
+    appId: "1:385838623346:web:18a5f52eb36c3406b230eb",
+    measurementId: "G-P3JE09TSFW"
+  };
